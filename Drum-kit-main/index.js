44354ed9f0d46016d690069d15c2ae72fc8f014d
satishkumar varma <satishkumarv752@gmail.com>
2023-08-04 05:50:04 +0000

    updated

--- Drum-kit-main/index.js
+++ Drum-kit-main/index.js
@@ -30,17 +30,17 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "a":
       var audio = new Audio('sounds/tom-1.mp3');
       audio.play();
       break;
 
-    case "a":
+    case "b":
       var audio = new Audio('sounds/tom-2.mp3');
       audio.play();
       break;
 
-    case "s":
+    case "c":
       var audio = new Audio('sounds/tom-3.mp3');
       audio.play();
       break;
@@ -50,21 +50,26 @@ function makeSound(key) {
       audio.play();
       break;
 
-    case "j":
+    case "e":
       var audio = new Audio('sounds/snare.mp3');
       audio.play();
       break;
 
-    case "k":
+    case "f":
       var audio = new Audio('sounds/crash.mp3');
       audio.play();
       break;
 
-    case "l":
+    case "g":
       var audio = new Audio('sounds/kick-bass.mp3');
       audio.play();
       break;
 
+      case "h":
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+  
     default:
       console.log(buttonInnerHTML);
   }
